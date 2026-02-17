cd6e857cd9d579fc51f0ae17ab871621ebbf081b
Raatwik <sathvik.novel@gmail.com>
2026-02-17 10:55:09 +0530

    Coded UC3 Banner Pattern

---
+++ OOPSBannerPattern.java
@@ -0,0 +1,12 @@
+public class OOPSBannerPattern{
+	public static void main(String[] args){
+	System.out.println(String.join("","   ***    ","   ***    ","***** ","  ***** "));
+	System.out.println(String.join(""," **   **  "," **   **  ","*   * "," **"));
+	System.out.println(String.join("","**     ** ","**     ** ","***** ","**"));
+	System.out.println(String.join("","**     ** ","**     ** ","**    "," *****"));
+	System.out.println(String.join("","**     ** ","**     ** ","**    ","     **"));
+	System.out.println(String.join(""," **   **  "," **   **  ","**    ","**   **"));
+	System.out.println(String.join("","   ***    ","   ***    ","**    "," *****"));
+	
+	}
+}
