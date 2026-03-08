dcde659568944877908d7907f25bca4cf4308804
Raatwik <sathvik.novel@gmail.com>
2026-03-08 15:29:36 +0530

    UC6

---
+++ OOPSBannerApp6.java
@@ -0,0 +1,52 @@
+public class OOPSBannerApp6 {
+
+    public static void main(String[] args) {
+
+        String[] O = buildO();
+        String[] P = buildP();
+        String[] S = buildS();
+
+        for (int i = 0; i < 7; i++) {
+            System.out.println(O[i] + "  " + O[i] + "  " + P[i] + "  " + S[i]);
+        }
+    }
+
+    // Build Letter O
+    public static String[] buildO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    // Build Letter P
+    public static String[] buildP() {
+        return new String[]{
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    // Build Letter S
+    public static String[] buildS() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*      ",
+                " ***** ",
+                "      *",
+                "*     *",
+                " ***** "
+        };
+    }
+}
